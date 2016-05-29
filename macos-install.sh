1acd9a8c5250caf0ff92cc58f9b85f600a8e6b5e
Thomas Belin <thomasbelin4@gmail.com>
2016-05-29 16:32:34 +0200

    [install] add the macos install script

---
+++ macos-install.sh
@@ -0,0 +1,4 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install tmux vim node postgresql brew-cask
+brew cask install iterm2 dropbox spotify
