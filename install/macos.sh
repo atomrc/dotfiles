88484c185c9934cbc369b727128711472ff3e56d
Thomas Belin <thomasbelin4@gmail.com>
2016-07-15 12:06:03 +0200

    [install] move install files in a specific folder

--- install/macos.sh
+++ install/macos.sh
@@ -1,4 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install tmux vim node postgresql brew-cask
+brew install tmux vim node postgresql ack brew-cask
 brew cask install iterm2 dropbox spotify firefox vlc
+
+npm install -g eslint eslint-config-defaults
